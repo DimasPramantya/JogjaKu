381a90c97cdaddc6dd8cbd63011dd1451a04975b
DimasPramantya <igkdimas@gmail.com>
2023-07-16 11:49:33 +0700

    registered some routes for sign up

--- BE/app.js
+++ BE/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const association = require('./util/dbAssociation');
 
+const userRoutes = require('./routes/user')
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/', userRoutes);
 
 association().then(()=>{
     app.listen(5000)
