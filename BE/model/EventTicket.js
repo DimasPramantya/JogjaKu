7bf43441951b9dfe9b1bdc1086ec1bf381033152
DimasPramantya <igkdimas@gmail.com>
2023-07-21 01:11:49 +0700

    added eventTickets model

---
+++ BE/model/EventTicket.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../util/db');
+
+const EventTicket = sequelize.define("eventTickets",{
+    id:{
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+    },
+    touristType:{
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    ageType:{
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    price:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+},{
+    timestamps: false
+})
+
+module.exports = EventTicket;
