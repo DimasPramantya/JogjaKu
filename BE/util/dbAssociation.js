7d894de595d63555ba08b3c0b97dcc40eb8fd98b
DimasPramantya <igkdimas@gmail.com>
2023-07-21 01:15:32 +0700

    added relation between ticket and event ticket

--- BE/util/dbAssociation.js
+++ BE/util/dbAssociation.js
@@ -1,9 +1,16 @@
+const Event = require("../model/Event");
+const EventTicket = require("../model/EventTicket");
 const Token = require("../model/Token");
 const User = require("../model/User");
 const sequelize = require("./db");
 
+//one to one relation between user and token
 User.hasOne(Token);
 Token.belongsTo(User);
+
+//one to many between event to event ticket
+Event.hasMany(EventTicket);
+EventTicket.belongsTo(Event);
 
 const association = async ()=>{
     try {
