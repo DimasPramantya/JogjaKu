4c44b503a84bb1e9572b7828bafd733396efa2af
DimasPramantya <igkdimas@gmail.com>
2023-07-21 01:34:05 +0700

    added error middleware handler

--- BE/app.js
+++ BE/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const association = require('./util/dbAssociation');
 
-const userRoutes = require('./routes/user')
+const userRoutes = require('./routes/user');
+const notFoundHandler = require('./middleware/errorNotFound');
+const errorBadRequestHandler = require('./middleware/badRequest');
 
 const app = express();
 
@@ -10,6 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', userRoutes);
+
+app.use(errorBadRequestHandler);
+
+app.use(notFoundHandler);
 
 association().then(()=>{
     app.listen(5000)
